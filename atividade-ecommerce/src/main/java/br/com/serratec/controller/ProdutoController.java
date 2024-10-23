a0b04d210d020f9284a69ab7ac6fa470e3999c69
Carla Daniele Ferreira <daniele.fersan@gmail.com>
2024-10-22 22:32:01 -0300

    Adaptações dia 22-10

--- atividade-ecommerce/src/main/java/br/com/serratec/controller/ProdutoController.java
+++ atividade-ecommerce/src/main/java/br/com/serratec/controller/ProdutoController.java
@@ -3,29 +3,31 @@ package br.com.serratec.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
-
-import br.com.serratec.dto.ProdutoRequestDTO;
-import br.com.serratec.dto.ProdutoResponseDTO;
 import br.com.serratec.entity.Produto;
-import br.com.serratec.service.ProdutoService;
+import br.com.serratec.repository.ProdutoRepository;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import jakarta.validation.Valid;
 
 @RestController
-@RequestMapping
+@RequestMapping("/produtos")
 public class ProdutoController {
 	
 	@Autowired
-	private ProdutoService service;
+	private ProdutoRepository repository;
 
 	@Operation(summary = "Lista todos os produtos", description = "A resposta lista os dados dos produtos id.")
 	@ApiResponses(value = { 
@@ -36,11 +38,12 @@ public class ProdutoController {
 			@ApiResponse(responseCode = "403", description = "Não há permissão para acessar o recurso"),
 			@ApiResponse(responseCode = "404", description = "Recurso não encontrado"),
 			@ApiResponse(responseCode = "505", description = "Exceção interna da aplicação") })
+	
+		
 	@GetMapping
-	public ResponseEntity<List<ProdutoResponseDTO>> listar() {
-		return ResponseEntity.ok(service.findAll());
-	}
-
+    public List<Produto> listar(){
+        return repository.findAll();
+    }	
 	
 	@Operation(summary = "Insere um novo produto", description = "A resposta retorna o nome e id.")
 	@ApiResponses(value = { 
@@ -51,13 +54,21 @@ public class ProdutoController {
 			@ApiResponse(responseCode = "403", description = "Não há permissão para acessar o recurso"),
 			@ApiResponse(responseCode = "404", description = "Recurso não encontrado"),
 			@ApiResponse(responseCode = "505", description = "Exceção interna da aplicação") })
-
+	
 	@PostMapping
-	public ResponseEntity<Object> inserir(@RequestBody ProdutoRequestDTO dto) {
-		ProdutoResponseDTO dtoResponse = service.inserir(dto);
-		return ResponseEntity.created(null).body(dtoResponse);
-	}
+    @ResponseStatus(HttpStatus.CREATED)
+    public Produto inserir(@Valid @RequestBody Produto p) {
+        return repository.save(p);
+    }
+	
+	@PutMapping("{id}")
+	public ResponseEntity<Produto> alterarProduto(@PathVariable Long id, @Valid @RequestBody Produto p) {
+		if (repository.existsById(id)) {
+			p.setId(id); // 
+			return ResponseEntity.ok(repository.save(p));
+		}
+		return ResponseEntity.notFound().build();	
+	}	
+}		
 
-}
-
-
+	

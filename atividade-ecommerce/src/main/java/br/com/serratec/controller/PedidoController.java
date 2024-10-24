47ce6620a96a50796c1d31be9f16432bd8958dfe
MariPuri5 <mpurificacao5@gmail.com>
2024-10-23 21:16:20 -0300

    Pedido

--- atividade-ecommerce/src/main/java/br/com/serratec/controller/PedidoController.java
+++ atividade-ecommerce/src/main/java/br/com/serratec/controller/PedidoController.java
@@ -1,11 +1,98 @@
 package br.com.serratec.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.serratec.dto.ClienteRequestDTO;
+import br.com.serratec.dto.ClienteResponseDTO;
+import br.com.serratec.dto.PedidoRequestDTO;
+import br.com.serratec.dto.PedidoResponseDTO;
+import br.com.serratec.entity.Cliente;
+import br.com.serratec.entity.Pedido;
+import br.com.serratec.service.ClienteService;
+import br.com.serratec.service.PedidoService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+
 @RestController
-@RequestMapping ("/pedidos")
+@RequestMapping("/pedidos")
 public class PedidoController {
-    
+//Origem service:
+	@Autowired
+	private PedidoService service;
 	
+//Metodos GET SET POST DELETE chamando o service
+	
+	//GET todos
+	@Operation(// documentação do get
+			summary = "Listar todos pedidos", description = "Retorna os dados dos pedidos")
+	@ApiResponses(value={//respostas das requisições
+			@ApiResponse(responseCode = "200", 
+			content = {@Content(schema = @Schema(implementation = Pedido.class), mediaType = "application/json")},
+			description = "Retorna todos os pedidos"),
+			@ApiResponse(responseCode = "401", description = "Erro de autenticação"),
+			@ApiResponse(responseCode = "403", description = "Não há permissão para acessar o recurso"),
+			@ApiResponse(responseCode = "404", description = "Recurso não encontrado"),
+			@ApiResponse(responseCode = "505", description = "Exceção interna da aplicação") 
+			}
+	)
+	@GetMapping
+	public ResponseEntity<List<PedidoResponseDTO>> listarTodos() {
+		return ResponseEntity.ok(service.listarTodosPedido());
+	}
+	
+	//GET por id
+	@Operation(// documentação do get
+			summary = "Listar pedido por id", description = "Retorna os dados do pedido")
+	@ApiResponses(value={//respostas das requisições
+			@ApiResponse(responseCode = "200", 
+			content = {@Content(schema = @Schema(implementation = Pedido.class), mediaType = "application/json")},
+			description = "Retorna o pedido solicitado"),
+			@ApiResponse(responseCode = "401", description = "Erro de autenticação"),
+			@ApiResponse(responseCode = "403", description = "Não há permissão para acessar o recurso"),
+			@ApiResponse(responseCode = "404", description = "Recurso não encontrado"),
+			@ApiResponse(responseCode = "505", description = "Exceção interna da aplicação") 
+			}
+	)
+	@GetMapping("{id}")
+	public ResponseEntity<ClienteResponseDTO> buscarId(@PathVariable Long id){
+		return ResponseEntity.ok(service.exibirPedido(id));
+	}
+	
+	
+	//POST inserir pedido
+	@Operation(summary = "Insere um novo pedido", description = "A resposta retorna o nome, email e telefone sem expor o id ou o cpf.")
+	@ApiResponses(value = { 
+			@ApiResponse(responseCode = "201", 
+			content = {@Content(schema = @Schema(implementation = Cliente.class), mediaType = "application/json")},
+			description = "Cliente cadastrado com sucesso"),
+			@ApiResponse(responseCode = "401", description = "Erro de autenticação"),
+			@ApiResponse(responseCode = "403", description = "Não há permissão para acessar o recurso"),
+			@ApiResponse(responseCode = "404", description = "Recurso não encontrado"),
+			@ApiResponse(responseCode = "505", description = "Exceção interna da aplicação") 
+			}
+	)
+	@PostMapping
+	public ResponseEntity<Object> inserirPedido(@RequestBody PedidoRequestDTO dto) { 
+		PedidoResponseDTO dtoResponse = service.inserirPedido(dto);
+		return ResponseEntity.created(null).body(dtoResponse);
+	}
+	
+	@PutMapping("/{id}")
+    public ResponseEntity<Object> atualizarPedido(@PathVariable Long id, @RequestBody PedidoRequestDTO pedidoDTO) {
+		ClienteResponseDTO pedidoAtualizado = service.atualizarPedido(id, pedidoDTO);
+        return ResponseEntity.ok(pedidoAtualizado);
+    }
 }
--- atividade-ecommerce/src/main/java/br/com/serratec/service/PedidoService.java
+++ atividade-ecommerce/src/main/java/br/com/serratec/service/PedidoService.java
@@ -1,10 +1,77 @@
 package br.com.serratec.service;
 
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import br.com.serratec.dto.PedidoRequestDTO;
+import br.com.serratec.dto.PedidoResponseDTO;
+import br.com.serratec.entity.Pedido;
+import br.com.serratec.enums.EnumPreparo;
+import br.com.serratec.enums.StatusPedido;
+import br.com.serratec.repository.PedidoRepository;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import br.com.serratec.repository.ClienteRepository;
 
 @Service
 public class PedidoService {
-	
-	
 
+    @Autowired
+    private PedidoRepository pedidoRepository;
+
+    @Autowired
+    private ClienteRepository clienteRepository;
+
+       
+    
+    // Inserir um novo pedido (POST)
+    public PedidoResponseDTO inserirPedido(PedidoRequestDTO requisicaoDTO) {
+        Pedido pedido = new Pedido();
+
+        // Definir o cliente do pedido (você pode ajustar conforme sua lógica de cliente)
+        pedido.setCliente(clienteRepository.findById(requisicaoDTO.getClienteId()).orElseThrow());
+
+     
+        pedido.setData(requisicaoDTO.getData());
+        pedido.setHora(requisicaoDTO.getHora());
+
+        
+        if (requisicaoDTO.getStatus() != null) {
+            pedido.setStatus(requisicaoDTO.getStatus());
+        } else {
+            pedido.setStatus(StatusPedido.REALIZADO); // Status padrão
+        }
+
+        
+        pedido = pedidoRepository.save(pedido);
+
+       
+        return new PedidoResponseDTO(pedido);
+    }
+
+    // Atualizar um pedido existente (PUT)
+    public PedidoResponseDTO atualizarPedido(Long id, PedidoRequestDTO pedidoModificado) {
+        Optional<Pedido> pedidoOptional = pedidoRepository.findById(id);
+        if (pedidoOptional.isEmpty()) {
+            throw new RuntimeException("Pedido não encontrado.");
+        }
+
+        Pedido pedido = pedidoOptional.get();
+        // Atualizar data e hora
+        pedido.setData(pedidoModificado.getData());
+        pedido.setHora(pedidoModificado.getHora());
+
+       
+        if (pedidoModificado.getStatus() != null) {
+            pedido.setStatus(pedidoModificado.getStatus());
+        }
+
+        
+        pedido = pedidoRepository.save(pedido);
+
+        
+        return new PedidoResponseDTO(pedido);
+    }
 }
